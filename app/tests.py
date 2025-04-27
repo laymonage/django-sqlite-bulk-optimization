29536bd7b949e34fcb5017b19734d03f81cb9c51
Sage Abdullah <me@laymonage.com>
2025-04-27 14:16:58 +0100

    Add test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from app.models import Note
+
 # Create your tests here.
+
+
+class NoteTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.notes = [
+            Note(title=f"Test Note {i}", content="This is a test note.")
+            for i in range(2**12)
+        ]
+
+    def test_note_creation(self):
+        with self.assertNumQueries(1):
+            Note.objects.bulk_create(self.notes)
